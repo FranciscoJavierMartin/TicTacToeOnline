2de73b83da71106d3c106f27bf0ca5d2cba205b6
Francisco <javiermartincuenca@gmail.com>
2019-04-23 22:07:54 +0200

    Login appear and dissapear

--- app/src/main/java/com/franciscomartin/tictactoe/LoginActivity.kt
+++ app/src/main/java/com/franciscomartin/tictactoe/LoginActivity.kt
@@ -2,11 +2,34 @@ package com.franciscomartin.tictactoe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        changeLoginFormVisibility(true)
+
+
+        buttonLogin.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+            changeLoginFormVisibility(false)
+
+        }
+    }
+
+    private fun changeLoginFormVisibility(showForm: Boolean){
+        if(showForm){
+            progressBarLogin.visibility = View.GONE
+            formLogin.visibility = View.VISIBLE
+        } else {
+            progressBarLogin.visibility = View.VISIBLE
+            formLogin.visibility = View.GONE
+        }
+
     }
 }
