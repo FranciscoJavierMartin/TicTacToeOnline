0dd72f57fddd7dbef10e6f5a2deadf22601c8438
Francisco <javiermartincuenca@gmail.com>
2019-04-27 13:40:58 +0200

    Animations added to loading game

--- app/src/main/java/com/franciscomartin/tictactoe/activities/GameActivity.kt
+++ app/src/main/java/com/franciscomartin/tictactoe/activities/GameActivity.kt
@@ -1,8 +1,8 @@
 package com.franciscomartin.tictactoe.activities
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
 import com.franciscomartin.tictactoe.R
 
 import kotlinx.android.synthetic.main.activity_game.*
@@ -12,12 +12,8 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
     }
 
 }
--- app/src/main/java/com/franciscomartin/tictactoe/activities/SearchGameActivity.kt
+++ app/src/main/java/com/franciscomartin/tictactoe/activities/SearchGameActivity.kt
@@ -1,16 +1,51 @@
 package com.franciscomartin.tictactoe.activities
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.franciscomartin.tictactoe.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_search_game.*
 
 class SearchGameActivity : AppCompatActivity() {
+
+    private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val user: FirebaseUser? = firebaseAuth.currentUser
+    private val uid: String? = user?.uid
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_game)
 
-        progressBarSearchGames.setIndeterminate(true)
+        this.changeMenuVisibility(true)
+
+        buttonPlay.setOnClickListener{
+            this.changeMenuVisibility(false)
+        }
+
+        buttonRanking.setOnClickListener {
+
+        }
+
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        this.changeMenuVisibility(true)
+    }
+
+    private fun changeMenuVisibility(showMenu: Boolean){
+        if(showMenu){
+            layoutProgressBar.visibility = View.GONE
+            layoutGameMenu.visibility = View.VISIBLE
+        } else {
+            layoutProgressBar.visibility = View.VISIBLE
+            layoutGameMenu.visibility = View.GONE
+        }
+    }
+
 }
