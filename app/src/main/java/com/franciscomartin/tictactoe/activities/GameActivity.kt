f3ab5ab77300bb0a409c4e3ee1e29d7ea5fedee5
Francisco <javiermartincuenca@gmail.com>
2019-04-27 19:24:10 +0200

    Game sreen designed

--- app/src/main/java/com/franciscomartin/tictactoe/activities/GameActivity.kt
+++ app/src/main/java/com/franciscomartin/tictactoe/activities/GameActivity.kt
@@ -1,18 +1,28 @@
 package com.franciscomartin.tictactoe.activities
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
 import com.franciscomartin.tictactoe.R
 
 import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
 
+    private val cells = arrayOf(
+        imageView0,
+        imageView1,
+        imageView2,
+        imageView3,
+        imageView4,
+        imageView5,
+        imageView6,
+        imageView7,
+        imageView8
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-
 
     }
 
